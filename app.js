ab316c1f9ce86885f4dcee6e69cecc2e69aeffe1
kafadarova <k.kafadarova@gmail.com>
2018-05-01 23:27:29 +0200

    adding an event listener- by clicking on roll button we generate a random number for the dice and get the rigth image representing the following number

--- app.js
+++ app.js
@@ -8,20 +8,38 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores= [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-//generating a random number between 1 and 6 - without adding 1 - between 0 and 5
-dice = Math.floor(Math.random() * 6) + 1;
-
 //with textContent Method we will change the content of the first player's score
 // dice - this is the variable which we declared at the beginning of our js file
-
-document.querySelector('#current' + activePlayer).textContent = dice;
+//document.querySelector('#current' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 //hide dice with the random number at the beginning changing the CSS property with DOM
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+//setting an event listener for the button roll
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+  //1.Random number
+  //generating a random number between 1 and 6 - without adding 1 - between 0 and 5
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+  //2.Display the result
+    var diceDOM =   document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+ dice + '.png';
+
+  //3.Update the round score - only IF the rolled number was NOT a 1
+
+});
