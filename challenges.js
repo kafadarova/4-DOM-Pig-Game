c3a55a175b57348b0e60f39dda6cca3ccade9fa4
kafadarova <k.kafadarova@gmail.com>
2018-05-04 22:33:06 +0200

    adding second dice in the html file and adjusting js file so the player will loose everything when one of the dices is a 1

--- challenges.js
+++ challenges.js
@@ -23,29 +23,41 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   if (gamePlaying){
     //1.Random number
     //generating a random number between 1 and 6 - without adding 1 - between 0 and 5
-      var dice = Math.floor(Math.random() * 6) + 1;
+      var dice1 = Math.floor(Math.random() * 6) + 1;
+      var dice2 = Math.floor(Math.random() * 6) + 1;
 
     //2.Display the result
-      var diceDOM =   document.querySelector('.dice');
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'dice-'+ dice + '.png';
+      document.getElementById('dice-1').style.display = 'block';
+      document.getElementById('dice-2').style.display = 'block';
+      document.getElementById('dice-1').src = 'dice-'+ dice1 + '.png';
+      document.getElementById('dice-2').src = 'dice-'+ dice2 + '.png';
 
-    //3.Update the round score - only IF the rolled number was NOT a 1
-      if ( dice === 6 && lastDice ===6){
-        //PLayer looses score
-        scores[activePlayer] = 0;
-        document.querySelector('#score-' + activePlayer).textContent = '0';
-        nextPlayer();
-      }else if (dice !== 1){;
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-      } else {
-        //Next player
-        nextPlayer();
-      }
+      //3.Update the round score - only IF the rolled number was NOT a 1
+          if (dice1 !== 1 && dice2 !==1){
+            //Add score
+          roundScore += dice1 + dice2;
+                  document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            } else {
+              //Next player
+              nextPlayer();
+          }
 
-      lastDice = dice;
+    // //3.Update the round score - only IF the rolled number was NOT a 1
+    //   if ( dice === 6 && lastDice ===6){
+    //     //PLayer looses score
+    //     scores[activePlayer] = 0;
+    //     document.querySelector('#score-' + activePlayer).textContent = '0';
+    //     nextPlayer();
+    //   }else if (dice !== 1){
+    //     //Add score
+    //     roundScore += dice;
+    //     document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    //   } else {
+    //     //Next player
+    //     nextPlayer();
+    //   }
+
+      // lastDice = dice;
     }
   });
 
@@ -57,11 +69,22 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         //Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+      //  Undefined, 0 , null or "" are coerced to false
+        //anything else is coerced to true
+        if(input) {
+            winningScore = input;
+        }else {
+          winningScore = 100;
+        }
         //Check if player won the game
-        if( scores[activePlayer] >= 100) {
+        if( scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+            document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else{
@@ -86,7 +109,8 @@ function nextPlayer(){
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 };
 
 // for init - dont use () !
@@ -97,8 +121,10 @@ function init(){
   activePlayer = 0;
   roundScore = 0;
   gamePlaying = true;
+
 //hide dice with the random number at the beginning changing the CSS property with DOM
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('dice-2').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
